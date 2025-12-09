71691153f2ff381e57d285ea57dc7403b6d1c012
alipala.ist@gmail.com <alipala.ist@gmail.com>
2025-12-09 21:10:39 +0100

    Remove sample files and test-import.js as it is no longer needed for component import testing.

--- test-import.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-try {
-  console.log('Testing import...');
-  const component = require('./src/components/CreateLearningPlanModal.tsx');
-  console.log('Component imported:', component);
-} catch (e) {
-  console.error('Import error:', e);
-}
